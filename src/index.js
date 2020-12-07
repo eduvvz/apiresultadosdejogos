c7b94d6660cfdb9046cbc5a3e83b79132aff7170
Eduardo Cerqueira <eduardo.silva@saudeid.com.br>
2020-12-07 14:39:42 -0300

    change port

--- src/index.js
+++ src/index.js
@@ -59,6 +59,6 @@ app.get('/', function(req, res) {
 });
 
 // Making Express listen on port 7000
-app.listen(7000, function() {
-  console.log('Running on port 7000.');
+app.listen(3000, function() {
+  console.log('Running on port 3000.');
 });
